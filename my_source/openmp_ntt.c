0b4ab65d368e87df1b99f062825e31efeecaf14e
Merve Karabulut <88487330+Mervespm@users.noreply.github.com>
2024-02-20 05:34:07 +0300

    Update openmp_ntt.c

--- my_source/openmp_ntt.c
+++ my_source/openmp_ntt.c
@@ -1,93 +1,101 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
-#include <omp.h>
 #include <time.h>
+#include <omp.h>
 
 #define N 256
 #define QINV -3327
 #define KYBER_Q 3329
 
-int16_t zetas[128] = {0}; // assuming this array is initialized elsewhere
+const int16_t zetas[128] = {
+  -1044,  -758,  -359, -1517,  1493,  1422,   287,   202,
+   -171,   622,  1577,   182,   962, -1202, -1474,  1468,
+    573, -1325,   264,   383,  -829,  1458, -1602,  -130,
+   -681,  1017,   732,   608, -1542,   411,  -205, -1571,
+   1223,   652,  -552,  1015, -1293,  1491,  -282, -1544,
+    516,    -8,  -320,  -666, -1618, -1162,   126,  1469,
+   -853,   -90,  -271,   830,   107, -1421,  -247,  -951,
+   -398,   961, -1508,  -725,   448, -1065,   677, -1275,
+  -1103,   430,   555,   843, -1251,   871,  1550,   105,
+    422,   587,   177,  -235,  -291,  -460,  1574,  1653,
+   -246,   778,  1159,  -147,  -777,  1483,  -602,  1119,
+  -1590,   644,  -872,   349,   418,   329,  -156,   -75,
+    817,  1097,   603,   610,  1322, -1285, -1465,   384,
+  -1215,  -136,  1218, -1335,  -874,   220, -1187, -1659,
+  -1185, -1530, -1278,   794, -1510,  -854,  -870,   478,
+   -108,  -308,   996,   991,   958, -1460,  1522,  1628
+};
 
+int16_t new_zetas[256][7]; // Make sure to initialize this array with the appropriate values
 
+int16_t montgomery_reduce(int32_t a) {
+    int16_t t = (int16_t)(a * QINV);
+    t = (int16_t)((a - t * KYBER_Q) >> 16);
+    return t;
+}
 
-void zeta_init(int32_t new_zetas[128][256], int16_t zetas[][256]) {
-    int16_t k = 0;
-    int16_t len_ = 128;
-    int16_t col = 0;
+void zeta_init() {
+    int k = 1;
+    int len_ = 128;
+    int col = 0;
     while (len_ >= 2) {
-        int16_t start = 0;
-        int16_t row = 1;
-        while (start < 256) {
-            int16_t *zeta = zetas[k++];
-            int16_t j = start;
+        int start = 0;
+        int row = 1;
+        while (start < N) {
+            int16_t zeta = zetas[k];
+            k += 1;
+            int j = start;
             while (j < start + len_) {
-                new_zetas[row][col] = (int32_t)zeta[j];
+                j += 1;
+                new_zetas[row][col] = zeta;
                 row += 2;
-                j++;
             }
             start = j + len_;
         }
         len_ >>= 1;
-        col++;
+        col += 1;
     }
 }
 
-int16_t montgomery_reduce_new(int32_t a) {
-    int32_t t = ((int64_t)a * QINV) >> 16;
-    t *= KYBER_Q;
-    int16_t res = (int16_t)(a - t);
-    return res;
-}
-void ntt_new(int16_t r[256]) {
-    int16_t len_ = 128;
-    int16_t cnt = 0;
+void ntt(int16_t r[N]) {
+    int k = 1;
+    int len_ = 128;
     while (len_ >= 2) {
-        int16_t start = 0;
+        int start = 0;
         #pragma omp parallel for
-        for (int16_t i = 0; i < 256; ++i) {
-            int32_t a = (int32_t)new_zetas[cnt][i] * r[i];
-            r[i] = (int16_t)a;
-        }
-        while (start < 256) {
-            #pragma omp parallel for
-            for (int16_t j = start; j < start + len_; ++j) {
-                int32_t t = montgomery_reduce_new((int32_t)new_zetas[cnt][j + len_] * r[j + len_]);
-                r[j + len_] = (int16_t)(r[j] - t);
-                r[j] = (int16_t)(r[j] + t);
+        for (start = 0; start < N; start += 2 * len_) {
+            int16_t zeta = zetas[k];
+            k += 1;
+            for (int j = start; j < start + len_; j++) {
+                int32_t t = montgomery_reduce(zeta * r[j + len_]);
+                r[j + len_] = r[j] - t;
+                r[j] = r[j] + t;
             }
-            start += 2 * len_;
         }
         len_ >>= 1;
-        cnt += 1;
     }
 }
-
 
 int main() {
     int16_t poly[N];
-    
-    
-    int res = 0;
-    double time_spent =0;
-    clock_t begin = clock();
-    clock_t end = clock();
-    int i, j;
+    // Initialize poly with your values or a random function
 
+    // Initialize new_zetas using zeta_init function
+    zeta_init();
 
-    for (i=0; i<1000; i++){
-		for (j=0; j<256; j++)
-			poly[j]= rand()%0x10000;
-		begin = clock();
-		ntt_new(poly);
-		end = clock();
-		time_spent += (double)(end - begin)/CLOCKS_PER_SEC;
-    
-	}
-	printf("Time in ms is %lf \n",time_spent/1000);
+    // Measure the execution time of the ntt function
+    clock_t start, end;
+    double cpu_time_used;
 
-    
+    start = clock();
+    for (int i = 0; i < 1000; i++) {
+        // Assuming poly is already initialized with values
+        ntt(poly);
+    }
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Execution time: %lf seconds\n", cpu_time_used);
 
     return 0;
 }
